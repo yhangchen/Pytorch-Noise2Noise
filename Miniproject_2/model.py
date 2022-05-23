7fcc2e0c6c8749b68c3946fbbae614256ba30519
CharlieLeee <lichengkun0805@gmail.com>
2022-05-23 12:14:39 +0200

    need further experimenting; stage code

--- Miniproject_2/model.py
+++ Miniproject_2/model.py
@@ -162,6 +162,7 @@ class Sequential(Module):
             param_idx += required_length
             model_idx += 1
     
+# Implemented for gradient testing
 class Linear(Module):
     def __init__(self, in_dim, out_dim, bias=True) -> None:
         super().__init__()
@@ -216,7 +217,7 @@ class Conv2d(Module):
         self.use_bias = bias
         
         # torch initialization
-        n = 5
+        n = in_channels * kernel_size**2
         stdv = 1. / math.sqrt(n)
         
         self.weight = empty(out_channels, in_channels, kernel_size, kernel_size).uniform_(-stdv, stdv)
@@ -326,12 +327,22 @@ class Upsample2d(Module):
 
 class Model():
     
-    def __init__(self) -> None:
+    def __init__(self, batch_size=4, lr=0.0001) -> None:
         
-        self.batch_size = 4
+        self.batch_size = batch_size
         
         self.device = device('cuda' if cuda.is_available() else 'cpu')
         
+        self.model = Sequential(
+            Conv2d(3, 32, 5, stride=2),
+            ReLU(), 
+            Conv2d(32, 32, 1, stride=2, padding=0),
+            ReLU(), 
+            Upsample2d(4, 32, 32, stride=2, kernel_size=5, padding=0),
+            ReLU(), 
+            Upsample2d(3, 32, 3, stride=1, kernel_size=5, padding=0),
+            sigmoid()
+        ).to(self.device)
         # Good case with small stride
         # self.model = Sequential(
         #     Conv2d(3, 32, 3, stride=1),
@@ -344,16 +355,16 @@ class Model():
         #     sigmoid()
         # ).to(self.device)
         ## Best model structure
-        self.model = Sequential(
-            Conv2d(3, 32, 3, stride=1),
-            ReLU(), 
-            Conv2d(32, 64, 3, stride=1, padding=3),
-            ReLU(), 
-            Upsample2d(2, 64, 32, stride=2),
-            ReLU(), 
-            Upsample2d(2, 32, 3, stride=2),
-            sigmoid()
-        ).to(self.device)
+        # self.model = Sequential(
+        #     Conv2d(3, 32, 3, stride=1),
+        #     ReLU(), 
+        #     Conv2d(32, 64, 3, stride=1, padding=3),
+        #     ReLU(), 
+        #     Upsample2d(2, 64, 32, stride=2),
+        #     ReLU(), 
+        #     Upsample2d(2, 32, 3, stride=2),
+        #     sigmoid()
+        # ).to(self.device)
         
         # Bad case
         # self.model = Sequential(
@@ -399,10 +410,10 @@ class Model():
         #     sigmoid()
         # ).to(self.device) 
         
-        self.optimizer = SGD(self.model.param(), 0.0001)
+        self.optimizer = SGD(self.model.param(), lr)
         self.criterion = MSELoss()
 
-        self.writer = SummaryWriter() if logged else None
+        self.writer = SummaryWriter(comment='lr_{}_bz_{}'.format(lr, self.batch_size)) if logged else None
 
     def load_pretrained_model(self, model_dir):
         params = pickle.load(open(model_dir, 'rb'))
@@ -414,7 +425,7 @@ class Model():
         pickle.dump(self.model.param(),open(dir, 'wb'))
             
     
-    def train(self, train_input, train_target, num_epoch=200, load_model=False):
+    def train(self, train_input, train_target, num_epoch=50, load_model=False):
         if load_model:
             self.load_pretrained_model('bestmodel.pth')
         
@@ -428,13 +439,16 @@ class Model():
                                  500 *
                                  self.batch_size,
                                  replace=False))
+            
             train_loader = self.load_dataset(train_input,
                                              train_target,
                                              sampler=train_sampler)
             trbar = tqdm(enumerate(train_loader),
                          total=len(train_loader),
                          unit='batch')
+            
             train_loss = 0.0
+            
             for batch_idx, (source, target) in trbar:
                 source, target = source.to(self.device), target.to(self.device)
                 denoise_source = self.model(source)
@@ -457,9 +471,12 @@ class Model():
             for batch_idx, (source, target) in valbar:
                 source, target = source.to(self.device), target.to(self.device)
                 denoised_source = self.model(source)
-                self.writer.add_images('Source/Val', source, epoch) if logged else None
-                self.writer.add_images('Target/Val', target, epoch) if logged else None
-                self.writer.add_images('Denoised/Val', denoised_source, epoch) if logged else None
+                
+                if logged:
+                    self.writer.add_images('Source/Val', source, epoch) 
+                    self.writer.add_images('Target/Val', target, epoch)
+                    self.writer.add_images('Denoised/Val', denoised_source, epoch) 
+                
                 loss = self.criterion(denoise_source, target)
                 val_loss += loss.item()
                 val_psnr_bth = 0
@@ -467,19 +484,22 @@ class Model():
                     val_psnr_bth += psnr(denoised_source[i], target[i])
                 val_psnr_bth /= self.batch_size
                 val_psnr += val_psnr_bth
-            self.writer.add_scalar('Loss/Val', val_loss /
-                                   (batch_idx + 1), epoch) if logged else None
-            self.writer.add_scalar('PSNR/Val', val_psnr /
-                                   (batch_idx + 1), epoch) if logged else None
             
+            # Mean PSNR of this batch
+            val_psnr = val_psnr / (batch_idx + 1)            
+            if logged:
+                self.writer.add_scalar('Loss/Val', val_psnr, epoch)
+                self.writer.add_scalar('PSNR/Val', val_psnr, epoch) 
+                
             if val_psnr > best_psnr:
                 best_psnr = val_psnr
                 print('New best_psnr: ', best_psnr/(batch_idx + 1))
                 print('Saving model....')
                 self.save_model('bestmodel.pth')
+                
     def predict(self, test_input):
         denoise_source = self.model(test_input)
-        return denoise_source
+        return denoise_source.mul(255.0)
 
     def load_raw(self, name):
         return load(os.path.join('./../data/', name))
@@ -494,6 +514,8 @@ class Model():
                           drop_last=True)
 
 if __name__ == '__main__':
-    model = Model()
-    train_input, train_target = model.load_raw('train_data.pkl')
-    model.train(train_input, train_target, load_model=False)
+    for bz in [16]:
+        for lr_test in [5e-5]:
+            model = Model(lr=lr_test, batch_size=bz)
+            train_input, train_target = model.load_raw('train_data.pkl')
+            model.train(train_input, train_target, load_model=False, num_epoch=300)
